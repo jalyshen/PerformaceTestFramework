8ae41aa1031463e2865fe628702faf8a1b60a310
Jaly <sjl.jaly@gmail.com>
2020-10-12 19:56:02 +0800

    根据JMeter的概念，修改配置对象的名称

--- src/main/java/org/jaly/pft/conf/Configuration.java
+++ src/main/java/org/jaly/pft/conf/Configuration.java
@@ -4,27 +4,27 @@ package org.jaly.pft.conf;
  * 配置信息对象
  *
  * snakeyaml这个包破坏了OO的封装，
- * 只能暂时把Users和Behavior对象变为独立的public class
+ * 只能暂时把ThreadGroup和Behavior对象变为独立的public class
  *
  * @author Jaly
  */
 public class Configuration {
-    private Users users;
+    private ThreadGroup threadGroup;
     private Behavior behavior;
 
     Configuration() {}
 
-    Configuration(Users users, Behavior behavior) {
-        this.users  = users;
+    Configuration(ThreadGroup threadGroup, Behavior behavior) {
+        this.threadGroup = threadGroup;
         this.behavior = behavior;
     }
 
-    public void setUsers(Users users) {
-        this.users = users;
+    public void setUsers(ThreadGroup threadGroup) {
+        this.threadGroup = threadGroup;
     }
 
-    public Users getUsers() {
-        return this.users;
+    public ThreadGroup getUsers() {
+        return this.threadGroup;
     }
 
     public void setBehavior(Behavior behavior) {
--- src/main/java/org/jaly/pft/conf/ThreadGroup.java
+++ src/main/java/org/jaly/pft/conf/ThreadGroup.java
@@ -1,11 +1,11 @@
 package org.jaly.pft.conf;
 
 /**
- * 线程（用户）相关设置
+ * 线程组（用户）相关设置
  *
  * @author Jaly
  */
-public class Users {
+public class ThreadGroup {
     /**
      * 设置同时请求的最大用户数量
      */
@@ -21,9 +21,9 @@ public class Users {
      */
     private int increase;
 
-    Users() {}
+    ThreadGroup() {}
 
-    Users(int max, int init, int increase) {
+    ThreadGroup(int max, int init, int increase) {
         this.max  = max;
         this.init = init;
         this.increase  = increase;
